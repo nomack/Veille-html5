150f38d26e541c7a016144a0e85ef4199cb03b44
Jean Boittiaux <nomack666@gmail.com>
2011-04-28 09:56:10 +0200

    Suprression des fichiers inutiles

--- websocket/startDaemon.php
+++
@@ -1,9 +0,0 @@
-<?php
-ob_implicit_flush(true);
-
-require_once 'socket.class.php';
-require_once 'socketWebSocket.class.php';
-require_once 'trigger.class.php';
-
-new socketWebSocket('localhost',8080);
-?>
